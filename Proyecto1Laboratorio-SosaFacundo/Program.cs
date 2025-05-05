a308a1387f02ab472dda04252d78df804512aac1
facundososaok <facundososaokk@gmail.com>
2025-05-05 19:33:40 -0300

    mi primer commit

--- Proyecto1Laboratorio-SosaFacundo/Program.cs
+++ Proyecto1Laboratorio-SosaFacundo/Program.cs
@@ -17,6 +17,7 @@ namespace Proyecto1Laboratorio_SosaFacundo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Hola Mundo");
         }
     }
 }
